bc6503f5fbc20d20faac8ee3e5aa42a2345f3ef1
Samuel <samrodri44@gmail.com>
2025-10-30 18:01:48 -0400

    initial commit: backend and frontend setup; built initial api for contacts

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #you may want to enable this for other use cases
+
+db = SQLAlchemy(app)
